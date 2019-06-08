06baaef9bb9ce6b67c7740e00cfbddd9a7cca49e
Josh Armentano <josh@abidewebdesign.com>
2019-06-07 17:39:03 -0700

    Changed link address.

--- template-parts/content-hero.php
+++ template-parts/content-hero.php
@@ -7,7 +7,7 @@
 			<div class="bg-img-content text-center d-inline-flex flex-column h-100 align-content-center justify-content-center">
 				<h1 class="text-white mb-2 py-3 px-5"><?php the_field('hero_banner_title'); ?></h2>
 				<h3 class="mb-1 mb-lg-0 text-white py-3 px-5 mx-lg-5"><?php the_field('header_banner_sub_title'); ?></h4>
-				<a href="<?php the_field('hero_banner_button_link'); ?>" class="btn btn-primary btn-white btn-lg mt-4"><span><?php the_field('hero_banner_button_label'); ?> <i class="fas fa-chevron-right ml-1"></i></span></a>
+				<a href="#" class="btn btn-primary btn-white btn-lg mt-4 inplayer-paywall-login"><span><?php the_field('hero_banner_button_label'); ?> <i class="fas fa-chevron-right ml-1"></i></span></a>
 			</div>
 		</div>
 	</div>
